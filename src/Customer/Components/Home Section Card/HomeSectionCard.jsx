5e29854d372dc4fd030c8b3a1ec7c934ea9ea1b8
Md Shahid Alam <md.shahidalam.cse24@heritageit.edu.in>
2024-01-19 17:55:43 +0530

    product card custom next and prev added

--- src/Customer/Components/Home Section Card/HomeSectionCard.jsx
+++ src/Customer/Components/Home Section Card/HomeSectionCard.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const HomeSectionCard = ({product}) => {
+const HomeSectionCard = ({ product }) => {
   return (
-    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3'>
-      
-
-        <div className='h-[13-rem] w-[10rem]'> 
-            <img src={product.image} className='object-cover object-top w-full h-full' alt="" />
-        </div>
-
-        <div className='p-4'>
-            <h3 className='text-lg font-medium text-gray-900'>{product.brand}</h3>
-            <p className='mt-2 text-sm text-gray-500'>{product.title}</p>
-        </div>
-
+    <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg w-[15rem] h-[18rem] mx-3">
+      <div className="h-[13-rem] w-[10rem] mx-3">
+        <img
+          src={product.image}
+          className="object-fit object-top w-full"
+          alt={product.title}
+        />
+      </div>
+      <div className="p-4">
+        <h3 className="text-lg font-medium text-gray-900">{product.brand}</h3>
+        <p className="mt-2 text-sm text-gray-500">{product.title}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeSectionCard
+export default HomeSectionCard;
--- src/Customer/Components/Home Section Card/HomeSectionCarousel.jsx
+++ src/Customer/Components/Home Section Card/HomeSectionCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "./HomeSectionCard";
 import { Button } from "@mui/material";
@@ -8,14 +8,47 @@ import { mens_kurta } from "../../Data/mens_kurta";
 import "react-alice-carousel/lib/alice-carousel.css";
 
 const HomeSectionCarousel = () => {
-  const [activeIdx, setActiveIdx] = useState(0);
-  const slidePrev = () => setActiveIdx(activeIdx - 1);
-  const slideNext = () => setActiveIdx(activeIdx + 1);
-  const syncActiveIdx = ({ item }) => setActiveIdx(item);
+  const renderNextButton = () => {
+    return (
+      <Button
+        className="z-50 bg-[]"
+        variant="contained"
+        sx={{
+          position: "absolute",
+          top: "8rem",
+          right: "0rem",
+          transform: "",
+          bgcolor: "white",
+        }}
+        aria-label="next"
+      >
+        <KeyboardArrowRightIcon sx={{ color: "black" }} />
+      </Button>
+    );
+  };
+
+  const renderPrevButton = () => {
+    return (
+      <Button
+        className="z-50 bg-[]"
+        variant="contained"
+        sx={{
+          position: "absolute",
+          top: "8rem",
+          left: "0rem",
+          transform: "",
+          bgcolor: "white",
+        }}
+        aria-label="prev"
+      >
+        <KeyboardArrowLeftIcon sx={{ color: "black" }} />
+      </Button>
+    );
+  };
 
   const responsive = {
-    0: { items: 1, itemsFit: "contain" },
-    720: { items: 3, itemsFit: "contain" },
+    0: { items: 2, itemsFit: "contain" },
+    568: { items: 3, itemsFit: "contain" },
     1024: { items: 5.5, itemsFit: "contain" },
   };
 
@@ -24,48 +57,18 @@ const HomeSectionCarousel = () => {
     .map((item) => <HomeSectionCard product={item} />);
 
   return (
-    <div className="relative px-4 lg:px-8">
-      <div className="relative p-5">
+    <div className="relative px-4 lg:px-8 sm:px-6">
+
+      <div className="relative p-5 border">
         <AliceCarousel
-          items={items}
-          disableButtonsControls
-          responsive={responsive}
+          mouseTracking
           disableDotsControls
-          activeIndex={activeIdx}
-          onSlideChanged={syncActiveIdx}
+          items={items}
+          responsive={responsive}
+          controlsStrategy="responsive"
+          renderPrevButton={renderPrevButton}
+          renderNextButton={renderNextButton}
         />
-
-        <Button
-          className="z-50"
-          variant="contained"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            right: "0rem",
-            transform: "",
-            bgcolor: "white",
-          }}
-          aria-label="next"
-          onClick={slideNext}
-        >
-          <KeyboardArrowRightIcon sx={{ color: "black" }} />
-        </Button>
-
-        <Button
-          className="z-50"
-          variant="contained"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            left: "0rem",
-            transform: "",
-            bgcolor: "white",
-          }}
-          aria-label="left"
-          onClick={slidePrev}
-        >
-          <KeyboardArrowLeftIcon sx={{ color: "black" }} />
-        </Button>
       </div>
     </div>
   );
